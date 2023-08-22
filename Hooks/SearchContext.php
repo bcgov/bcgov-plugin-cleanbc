fe423965527be55b3ac9fea7d1a41fbd1f003bd2
Nate King <codewise.nate@gmail.com>
2023-08-22 16:33:25 -0700

    1.0.7 DESCW-1391 Modify search details

---
+++ Hooks/SearchContext.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bcgov\Plugin\CleanBC\Hooks;
+
+/**
+ * The SearchContext class provides methods for modifying the search query context in WordPress.
+ *
+ * @since 1.0.7
+ * 
+ * @package Bcgov\Plugin\CleanBC
+ */
+class SearchContext
+{
+	/**
+	 * Include which post types are included in search results. 
+	 *
+	 * @since 1.0.7
+	 *
+	 * @return void
+	 */
+	public function bcgov_included_post_types_in_search($query) {
+		if ($query->is_search() && !is_admin() && $query->is_main_query()) {
+			$query->set('post_type', ['page', 'post']);
+		}
+	}
+
+	/**
+	 * Modify search results post date to include the post type.
+	 *
+	 * @since 1.0.7
+	 *
+	 * @param string $date The original post date of the search result.
+	 * @param WP_Post $post The post object.
+	 * @return string Modified post date.
+	 */
+	public function bcgov_modify_search_result_date($date, $post) {
+		if (is_search()) {
+			$post_type = get_post_type($post);
+			if ($post_type) {
+				$date = "$date | $post_type";
+			}
+		}
+		return $date;
+	}
+}
--- Setup.php
+++ Setup.php
@@ -5,6 +5,7 @@ namespace Bcgov\Plugin\CleanBC;
 use Bcgov\Plugin\CleanBC\Hooks\{
     EnqueueAndInject,
     EnableVueApp,
+    SearchContext,
 };
 
 /**
@@ -24,11 +25,14 @@ class Setup
 
         $plugin_enqueue_and_inject = new EnqueueAndInject();
         $plugin_enable_vue_app = new EnableVueApp();
+        $plugin_search = new SearchContext();
 
         // Filters.
         add_filter('wp_theme_json_data_theme', [$plugin_enqueue_and_inject, 'filter_theme_json_theme_plugin']);
+        add_filter('get_the_date', [$plugin_search, 'bcgov_modify_search_result_date'], 10, 2);
 
         // Actions.
+        add_action('pre_get_posts', [$plugin_search, 'bcgov_included_post_types_in_search']);
         add_action('wp_enqueue_scripts', [$plugin_enqueue_and_inject, 'bcgov_plugin_enqueue_scripts']);
         add_action('admin_enqueue_scripts', [$plugin_enqueue_and_inject, 'bcgov_plugin_enqueue_admin_scripts']);
         add_action('enqueue_block_editor_assets', [$plugin_enable_vue_app, 'vuejs_wordpress_block_plugin']);
