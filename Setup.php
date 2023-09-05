5b4c5e1f8879686c92063c9a520e6feafd0e3073
Nate King <codewise.nate@gmail.com>
2023-09-05 09:29:52 -0700

    DESCW-1477 adds body class + search toggle fix

--- Setup.php
+++ Setup.php
@@ -28,9 +28,10 @@ class Setup
         $plugin_search = new SearchContext();
 
         // Filters.
-        add_filter('wp_theme_json_data_theme', [$plugin_enqueue_and_inject, 'filter_theme_json_theme_plugin']);
-        add_filter('get_the_date', [$plugin_search, 'bcgov_modify_search_result_date'], 10, 2);
+        add_filter( 'wp_theme_json_data_theme', [$plugin_enqueue_and_inject, 'filter_theme_json_theme_plugin'] );
+        add_filter( 'get_the_date', [$plugin_search, 'bcgov_modify_search_result_date'], 10, 2 );
         add_filter( 'block_categories_all', [$plugin_enable_vue_app, 'custom_block_categories'], 10, 2 );
+        add_filter( 'body_class', [ $plugin_enqueue_and_inject, 'add_cleanbc_class_to_body' ] );
 
         // Actions.
         add_action('pre_get_posts', [$plugin_search, 'bcgov_included_post_types_in_search']);
--- scripts/public/search.js
+++ scripts/public/search.js
@@ -21,62 +21,59 @@ const bcgovBlockThemePlugin = {
 
 					const searchInput = qs('input', searchFieldContainer);
 					const searchButton = qs('button', searchFieldContainer);
-				}
-
-				if (searchFieldContainer && siblingElement) {
-					siblingElement.parentNode.insertBefore(searchFieldContainer, siblingElement);
-				}
-
-				toggleSearchBtn.addEventListener('click', function (event) {
-
-					event.preventDefault();
+					
+					if (searchFieldContainer && siblingElement) {
+						siblingElement.parentNode.insertBefore(searchFieldContainer, siblingElement);
+					}
+					toggleSearchBtn.addEventListener('click', function (event) {
+	
+						event.preventDefault();
+	
+						if (searchFieldContainer) {
+	
+							if (searchFieldContainer.classList.contains('hidden')) {
+								searchFieldContainer.classList.remove('hidden');
+								if (searchInput) {
+									searchInput.focus();
+								}
+							} else {
+								searchFieldContainer.classList.add('hidden');
+							}
+						}
+					});
+	
+					toggleSearchBtn.addEventListener('keydown', (event) => {
+						if (event.code === 'Space' || event.code === 'Enter') {
+							event.preventDefault();
+							toggleSearchBtn.click();
+						}
+					});
 
 					if (searchFieldContainer) {
 
-						if (searchFieldContainer.classList.contains('hidden')) {
-							searchFieldContainer.classList.remove('hidden');
-							if (searchInput) {
-								searchInput.focus();
-							}
-						} else {
-							searchFieldContainer.classList.add('hidden');
-						}
+						searchInput.addEventListener('blur', function (event) {
+							event.preventDefault();
+							requestAnimationFrame(() => {
+								if (searchButton === document.activeElement) return;
+								if (toggleSearchBtn === document.activeElement) return;
+								toggleSearchBtn.focus();
+								toggleSearchBtn.click();
+							});
+						});
+		
+						searchButton.addEventListener('blur', function (event) {
+							event.preventDefault();
+							requestAnimationFrame(() => {
+								if (searchInput === document.activeElement) return;
+								if (toggleSearchBtn === document.activeElement) return;
+								toggleSearchBtn.focus();
+								toggleSearchBtn.click();
+							});
+						});
+		
 					}
-				});
-
-				toggleSearchBtn.addEventListener('keydown', (event) => {
-					if (event.code === 'Space' || event.code === 'Enter') {
-						event.preventDefault();
-						toggleSearchBtn.click();
-					}
-				});
-
+				}
 			}
-
-			if (searchFieldContainer) {
-
-				searchInput.addEventListener('blur', function (event) {
-					event.preventDefault();
-					requestAnimationFrame(() => {
-						if (searchButton === document.activeElement) return;
-						if (toggleSearchBtn === document.activeElement) return;
-						toggleSearchBtn.focus();
-						toggleSearchBtn.click();
-					});
-				});
-
-				searchButton.addEventListener('blur', function (event) {
-					event.preventDefault();
-					requestAnimationFrame(() => {
-						if (searchInput === document.activeElement) return;
-						if (toggleSearchBtn === document.activeElement) return;
-						toggleSearchBtn.focus();
-						toggleSearchBtn.click();
-					});
-				});
-
-			}
-
 		});
 	}
 }
