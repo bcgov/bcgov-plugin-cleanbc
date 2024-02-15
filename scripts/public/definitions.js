db524b30487f8b80d091ad4616d931687b22059d
Nate King <Nate.King@gov.bc.ca>
2024-02-15 15:31:32 -0800

    CLEANBC-173 catch error disable lint no-console

--- scripts/public/definitions.js
+++ scripts/public/definitions.js
@@ -30,64 +30,83 @@ const bcgovBlockThemePluginDefnitions = () => {
 				dialog.close();
 			} );
 
-			definitionLinks.forEach(function(link) {
-				link.classList.add('icon-definition');
-				link.setAttribute('aria-label', 'opens definition dialog for: ' + link.text);
-			
+			definitionLinks.forEach( function ( link ) {
+				link.classList.add( 'icon-definition' );
+				link.setAttribute(
+					'aria-label',
+					'opens definition dialog for: ' + link.text
+				);
+
 				// Adding event listeners for both click and keypress events
-				addEventListeners(link);
-			});
-			
-			function addEventListeners(element) {
-				element.addEventListener('click', handleClick);
-				element.addEventListener('keypress', handleKeypress);
+				addEventListeners( link );
+			} );
+
+			function addEventListeners( element ) {
+				element.addEventListener( 'click', handleClick );
+				element.addEventListener( 'keypress', handleKeypress );
 			}
-			
-			async function handleClick(event) {
-				if (event.type === 'click' || (event.type === 'keypress' && event.key === 'Enter')) {
+
+			async function handleClick( event ) {
+				if (
+					event.type === 'click' ||
+					( event.type === 'keypress' && event.key === 'Enter' )
+				) {
 					event.preventDefault();
-					const url = this.getAttribute('href');
-					const cachedData = window.sessionStorage.getItem(url);
-			
-					if (cachedData) {
-						const { title, content } = JSON.parse(cachedData);
-						displayContent(title, content);
+					const url = this.getAttribute( 'href' );
+					const cachedData = window.sessionStorage.getItem( url );
+
+					if ( cachedData ) {
+						const { title, content } = JSON.parse( cachedData );
+						displayContent( title, content );
 					} else {
 						try {
-							const response = await fetch(url);
+							const response = await fetch( url );
 							const html = await response.text();
 							const parser = new window.DOMParser();
-							const doc = parser.parseFromString(html, 'text/html');
-							const title = doc.querySelector('h1.wp-block-post-title').innerText;
-							const content = doc.querySelector('.entry-content').innerHTML;
+							const doc = parser.parseFromString(
+								html,
+								'text/html'
+							);
+							const title = doc.querySelector(
+								'h1.wp-block-post-title'
+							).innerText;
+							const content =
+								doc.querySelector( '.entry-content' ).innerHTML;
 							const dataToCache = { title, content };
-			
-							window.sessionStorage.setItem(url, JSON.stringify(dataToCache));
-							displayContent(title, content);
-						} catch (error) {
-							console.error('Error fetching content:', error);
+
+							window.sessionStorage.setItem(
+								url,
+								JSON.stringify( dataToCache )
+							);
+							displayContent( title, content );
+						} catch ( error ) {
+							/* eslint-disable no-console */
+							console.error( 'Error fetching content:', error );
+							/* eslint-enable no-console */
 						}
 					}
 				}
 			}
-			
-			function handleKeypress(event) {
+
+			function handleKeypress( event ) {
 				if ( event.key === 'Enter' || event.keycode === 13 ) {
-					handleClick(event);
+					handleClick( event );
 				}
 			}
-			
-			function displayContent(title, content) {
-				const dialogContent = document.querySelector('#dialog .dialog-content');
-				dialogContent.innerHTML = '<h2 tabindex="0">' + title + '</h2>' + content;
+
+			function displayContent( title, content ) {
+				const dialogContent = document.querySelector(
+					'#dialog .dialog-content'
+				);
+				dialogContent.innerHTML =
+					'<h2 tabindex="0">' + title + '</h2>' + content;
 				showDialog();
-				dialogContent.querySelector('h2').focus();
+				dialogContent.querySelector( 'h2' ).focus();
 			}
-			
+
 			function showDialog() {
 				dialog.showModal();
 			}
-			
 		}
 	} );
 };
